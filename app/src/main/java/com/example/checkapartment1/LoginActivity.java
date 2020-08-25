6ec023fd4bdf7ec5fb5d01727083d0e948d06ce2
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-08-24 21:28:28 -0400

    Actividad principal quedo seteada en el Login activity_login.xml

---
+++ app/src/main/java/com/example/checkapartment1/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.checkapartment1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText etn, etp;
+    private String popup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etn = findViewById(R.id.et2);
+        etp = findViewById(R.id.et1);
+
+    }
+    public void login(View view){
+        //almaceno lo que el usuario escribe en el campo de la vista
+        String email=etn.getText().toString();
+        String pass=etp.getText().toString();
+
+        if(email.length() == 0){
+            popup="Corrige lo siguiente:\nEscribe un email";
+            Toast.makeText(this,popup, Toast.LENGTH_LONG).show();
+
+        }
+        if(pass.length()==0){
+            popup=popup + "\nEscribe una contraseña";
+            Toast.makeText(this,popup, Toast.LENGTH_LONG).show();
+            popup="";
+
+        }
+        if(email.length()!=0 && pass.equals("123Pass")){
+            Toast.makeText(this,"Bienvenido denuevo"+ email, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
